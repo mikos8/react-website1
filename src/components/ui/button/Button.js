cd4f6645b4b7c0a4450f38d421f6ee747d634603
Miras Zhiyenbayev <65290147+mikos8@users.noreply.github.com>
2021-09-07 18:58:05 +0600

    add button

---
+++ src/components/ui/button/Button.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Button.css";
+
+const STYLES = ["btn--primary", "btn--outline"];
+const SIZES = ["btn--medium", "btn--large", "btn--mobile", "btn--wide"];
+const COLOR = ["primary", "blue", "red", "green"];
+
+export const Button = ({
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize,
+  buttonColor,
+}) => {
+  return (
+    <button
+      className={`btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonColor}`}
+     onClick={onClick} type={type}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
